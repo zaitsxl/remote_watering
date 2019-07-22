f95be7c061e941c37afe2169bd3d8d4654e3b0ee
Alexander Zaitsev <zaits.xl@gmail.com>
2019-07-22 21:51:27 +0100

    Added power monitor

--- control.php
+++ control.php
@@ -4,15 +4,27 @@
 </head>
 <body bgcolor="grey">
 <p align="center"><img src="/webcam/?action=stream"/>
+<p align="center"><font size="30">Water: 
 <?php
   $valve_status = shell_exec("sudo /usr/bin/python /var/www/html/relay.py status");
   if ($valve_status == 1): ?>
-     <p align="center"><font size="30">Water is ON</font>
+     <font color="red">ON</font>
   <?php elseif ($valve_status == 0): ?>
-     <p align="center"><font size="30">Water is OFF</font>
+     <font color="green">OFF</font>
   <?php else: ?>
-     <p align="center"><font size="30" color="red">Error getting valve status</font>
+     <font color="red">ERROR</font>
 <?php endif; ?>
+<p align="center"><font size="30">Power:
+<?php
+     $power_status = shell_exec("sudo /sbin/apcaccess | grep STATUS | awk '{ print $3 }'");
+     if (strpos($power_status, "ONLINE") !== false): ?>
+     <font color="green">LINE
+     <?php elseif (strpos($power_status, "ONBATT") !== false): ?>
+     <font color="red">BATTERY
+     <?php else: ?>
+     <font color="red">ERROR
+<?php endif;
+     echo "</font>"; ?>
 <form method="post" action="trigger.php">
         <h1 align="center"><font size="40">
 	<p><input type="radio" value="on" name="control" style="width: 20em; height: 20em" />ON

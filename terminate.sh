75d3e8297e12a5469c5ff0f806250e9a900128cc
Alexander Zaitsev <zaits.xl@gmail.com>
2019-08-27 10:41:10 +0100

    Added connection watchdog

--- terminate.sh
+++ terminate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 CHG=`/sbin/apcaccess | /bin/grep BCHARGE | /usr/bin/awk '{ print $3 }' | /usr/bin/awk -F. '{ print $1 }'`
-if [ $CHG -le 100 ]
+if [ $CHG -le 50 ]
 	then /usr/bin/python relay.py off
+fi
+
+ping 192.168.1.1 -c 1 > /dev/null
+if [ ! -z "$?" ]
+	then /sbin/reboot
 fi
